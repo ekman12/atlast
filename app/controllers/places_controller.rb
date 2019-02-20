5b348289687c39dc488086d2972300d233f68937
vilson junior <vilsonrjunior@gmail.com>
2019-02-20 19:25:51 +0800

    first commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,10 @@
 class PlacesController < ApplicationController
   skip_before_action :authenticate_user!
   def index
+    @place = Place.all
   end
 
   def show
+   @place = Place.find(params[:id])
   end
 end
