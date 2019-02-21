42a0a23b284bc059aab6495ac3df112579e8a5ef
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-21 10:55:04 +0800

    place to tags relationship

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   has_many :posts
+  has_many :users, through: :posts
+  has_many :post_tags, through: :posts
+  has_many :tags, through: :post_tags
   has_many :wishlist_items
 end
