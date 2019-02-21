1aacd62b1830aba2b5f93b7f55cba035eb215d43
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-21 15:11:29 +0800

    created new post journey. If place does not exist it creates. If it does - it updates it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,25 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    user = current_user
+    place_name = post_params[:place]
+    place = Place.find_by(name: place_name)
+    place = Place.create(name: place_name) if place.nil?
+    @post = Post.new(
+      photo: post_params[:photo],
+      note: post_params[:note],
+      place: place,
+      user: current_user
+      )
+    if @post.save
+      redirect_to places_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -18,4 +34,11 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:photo, :note, :place)
+  end
+
 end
