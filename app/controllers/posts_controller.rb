270248b4e70de592feb7285cf51ded751e9509eb
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-21 16:31:09 +0800

    when creating a new post you now also create new post tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,25 +8,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    # raise
     @post = Post.new
+    @venue_tags = Tag.where(tag_type: "venue")
+    @meal_tags = Tag.where(tag_type: "meal")
+    @vibe_tags = Tag.where(tag_type: "vibe")
+    @food_tags = Tag.where(tag_type: "food")
   end
 
   def create
-    user = current_user
-    place_name = post_params[:place]
-    place = Place.find_by(name: place_name)
-    place = Place.create(name: place_name) if place.nil?
+    @tag_ids = params[:post][:tag_ids]
+    place = Place.find_by(name: post_params[:place])
+    place = Place.create(name: post_params[:place]) if place.nil?
     @post = Post.new(
       photo: post_params[:photo],
       note: post_params[:note],
       place: place,
       user: current_user
-      )
+    )
     if @post.save
       redirect_to places_path
     else
       render :new
     end
+    create_post_tags
   end
 
   def update
@@ -38,7 +43,14 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:photo, :note, :place)
+    params.require(:post).permit(:photo, :note, :place, :tags)
   end
 
+  def create_post_tags
+    @tag_ids = @tag_ids - [""]
+    @post_tags = []
+    @tag_ids.each do |tag|
+      PostTag.create(tag_id: tag.to_i, post: @post)
+    end
+  end
 end
