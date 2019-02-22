2320afc9ed69686be5f05dd948a5d5085dc2207f
vilson junior <vilsonrjunior@gmail.com>
2019-02-22 15:05:47 +0800

    fixed infowindows so when a new one is open, the previous closes automatically

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -6,19 +6,42 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
 
+  // var infowindow = new google.maps.InfoWindow({
+  //   content: contentString
+  // });
 
-  markers.forEach((marker) => {
-    var contentString = marker["infoWindow"]["content"];
-    var marker = map.addMarker({
-      position: {lat: marker["lat"], lng: marker["lng"]}
-    });
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-  });
+  const markersArr = markers.map(marker => {
+    return {
+      lat: marker["lat"],
+      lng: marker["lng"],
+      infoWindow: {
+        content: marker["infoWindow"]["content"]
+      }
+    }
+  })
+
+  // console.log(markersArr)
+
+  map.addMarkers(markersArr)
+
+  // console.log(markersArr)
+
+  // map.markers.forEach((marker) => {
+  //   // var contentString = marker["infoWindow"]["content"];
+  //   // var marker = map.addMarker({
+  //   //   position: {lat: marker["lat"], lng: marker["lng"]}
+  //   // });
+  //   // var infowindow = new google.maps.InfoWindow({
+  //   //   content: contentString
+  //   // });
+  //   marker.addListener('click', function() {
+  //     console.log(map);
+  //     console.log(marker.infoWindow);
+  //     // map.hideInfoWindows()
+  //     marker.infoWindow.open(map,marker);
+  //   })
+  // });
+
 
   // map.addMarkers(markers);
 
