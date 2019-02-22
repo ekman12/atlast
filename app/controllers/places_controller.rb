032851d6006f542881577cf86b2ff2fe6aa7a35a
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-23 07:39:21 +0800

    refactored. separated tag types

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,9 +2,8 @@ class PlacesController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @tags = Tag.all
+    define_tags
     @places = current_user.place_feed
-
     place_search if params[:query].present?
 
     @searched_tags = []
@@ -30,10 +29,8 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     @post = Post.new
     @user = current_user
-    # @post_tags = Post.find(params[:id])
     @allposts = Post.where(place: @place)
     @posts = []
-    # raise
     @allposts.each do |post|
       @posts.push(post) if current_user.following.include?(post.user)
     end
@@ -41,13 +38,21 @@ class PlacesController < ApplicationController
 
   private
 
+  def define_tags
+    @tags = Tag.all
+    @venue_tags = Tag.where(tag_type: "venue")
+    @meal_tags = Tag.where(tag_type: "meal")
+    @vibe_tags = Tag.where(tag_type: "vibe")
+    @food_tags = Tag.where(tag_type: "food")
+  end
+
+
   def place_search
     @matching_places = Place.search_by_place(params[:query])
     @places = @places & @matching_places
   end
 
   def multiple_tag_search
-    # Populate the array with results from the first filter
     @tag_results = Place.search_by_tag_id(@searched_tags.first)
     @searched_tags.each do |id|
       # on the first search there is no change as defined before loop
--- app/javascript/packs/tag-selector.js
+++ app/javascript/packs/tag-selector.js
@@ -1,5 +1,3 @@
-// document.querySelector("#tag-selector")
-
 const tagSelector = () => {
   const tagSelectors = document.querySelectorAll('[data-tagid]');
   tagSelectors.forEach((tag) => {
@@ -10,23 +8,4 @@ const tagSelector = () => {
   }
 )};
 
-// export { tagSelector };
-
-// const tagSelector = () => {
-//   let tag = document.querySelector('[data-tagid]')
-//   tag.addEventListener("click", (event) => {
-//     console.log(event.currentTarget);
-//     event.currentTarget.classList.toggle("btn-tag");
-//   });
-// }
-
 export { tagSelector };
-
-// const clickWishlistHeart = () => {
-//   const wishlistHeart = document.getElementById("wishlist-heart")
-//   wishlistHeart.addEventListener("click", (event) => {
-//     event.currentTarget.classList.toggle("wishlist-heart-clicked");
-//   });
-// };
-
-// export { clickWishlistHeart };
