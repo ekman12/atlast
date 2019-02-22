c7da6b448f1aa133ed46eb9abe93add8e4dc1077
vilson junior <vilsonrjunior@gmail.com>
2019-02-22 13:54:31 +0800

    added infowindows to markers - styling pending

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,15 +3,14 @@ class PlacesController < ApplicationController
 
   def index
     @places = current_user.place_feed
-    @filtered_places = @places.where.not(latitude: nil, longitude: nil)
-
-    @markers = @filtered_places.map do |place|
+    @filtered_places = []
+    @places.each { |p| @filtered_places << p if p.latitude && p.longitude}
+    # @filtered_places = @places.where.not(latitude: nil, longitude: nil)
+     @markers = @filtered_places.map do |place|
       {
         lat: place.latitude,
-        lng: place.longitude
-        # infoWindow: { content: render_to_string(partial: "/place/map", locals: { place: place }) }
-        # Uncomment the above line if you want each of your markers to display a info window when clicked
-        # (you will also need to create the partial "/flats/map_box")
+        lng: place.longitude,
+        infoWindow: { content: render_to_string(partial: "infowindow", locals: { place: place }) }
       }
     end
   end
@@ -20,6 +19,13 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     @post = Post.new
     @user = current_user
-    @post_tags = Post.find(params[:id])
+    # @post_tags = Post.find(params[:id])
+    @allposts = Post.where(place: @place)
+    @posts = []
+    # raise
+    @allposts.each do |post|
+      @posts.push(post) if current_user.following.include?(post.user)
+    end
   end
 end
+
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,10 +1,27 @@
 import GMaps from 'gmaps/gmaps.js';
 
 const mapElement = document.getElementById('map');
+
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
+
+
+  markers.forEach((marker) => {
+    var contentString = marker["infoWindow"]["content"];
+    var marker = map.addMarker({
+      position: {lat: marker["lat"], lng: marker["lng"]}
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  });
+
+  // map.addMarkers(markers);
+
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
