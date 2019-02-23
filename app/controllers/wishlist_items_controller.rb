b48f7d9990b85413d4b7291a75c3ff8d11a713b1
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-23 16:14:55 +0800

    add to wishlist on place show is done

--- app/controllers/wishlist_items_controller.rb
+++ app/controllers/wishlist_items_controller.rb
@@ -10,7 +10,29 @@ class WishlistItemsController < ApplicationController
   end
 
   def create
-    raise
+    # raise
     @place = Place.find(params[:place_id])
+    @wishlist_check = WishlistItem.where(place_id: @place.id, user_id: current_user.id)
+    if @wishlist_check.empty?
+      @wishlist = WishlistItem.new
+      @wishlist.user = current_user
+      @wishlist.place = @place
+      if @wishlist.save
+        respond_to do |format|
+          # raise
+          format.html { redirect_to place_path(@place) }
+          format.js # <-- will render `app/views/reviews/create.js.erb`
+        end
+      else
+        render :new
+      end
+    else
+      @wishlist_check.destroy_all
+      respond_to do |format|
+        # raise
+        format.html { redirect_to place_path(@place) }
+        format.js # <-- will render `app/views/reviews/create.js.erb`
+      end
+    end
   end
 end
