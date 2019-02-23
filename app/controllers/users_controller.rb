2fa6ff782e6afb5da56e8ca914b99fe244c98f9c
Caio Abibe <caio.abibe@gmail.com>
2019-02-23 15:52:16 +0800

    implemented, friends search bar on friends search page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,15 @@ class UsersController < ApplicationController
   # skip_before_action :authenticate_user!
 
   def index
-    @followings = current_user.following
-    @users = User.all
-    @nonfollowings = @users - @followings
+    if params[:query].present?
+      @users = User.search_by_user(params[:query])
+        @followings = current_user.following.search_by_user(params[:query])
+      @nonfollowings = @users - @followings
+    else
+      @followings = current_user.following
+      @users = User.all
+      @nonfollowings = @users - @followings
+    end
   end
 
   def show
