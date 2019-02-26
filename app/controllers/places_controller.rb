d1c4010f65fd4bb30b0b6ba37dee85060b95f492
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-26 13:40:34 +0800

    tag search refactored

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,33 +1,15 @@
 class PlacesController < ApplicationController
-  # before_action :authenticate_user!
-
   def index
     define_tags
-    # @places = []
-    # @posts = current_user.post_feed
-    # @posts.each do |post|
-    #   @places << post.place
-    # end
-    # @places
-
-    # DO POSTS NOT PLACES BECAUSE A USER COULD
-
     @places = current_user.place_feed
     place_search if params[:query].present?
 
-    @searched_tags = []
-
     if params[:tags].present?
-      params[:tags].keys.each do |id|
-        @searched_tags << id.to_i
-      end
+      @searched_tags = params[:tags].keys
+      multiple_tag_search
     end
 
-    # raise
-    multiple_tag_search unless @searched_tags.empty?
-
     @filtered_places = []
-    # raise
     @places.each { |p| @filtered_places << p if p.latitude && p.longitude }
     @markers = @filtered_places.map do |place|
       {
@@ -56,29 +38,17 @@ class PlacesController < ApplicationController
     @good_for_tags = Tag.where(tag_type: "good_for")
   end
 
-
   def place_search
     @matching_places = Place.search_by_place(params[:query])
     @places = @places & @matching_places
   end
 
   def multiple_tag_search
-    @tag_results = Post.search_by_tag_name(Tag.find(@searched_tags.first).name)
-    @searched_tags.each do |id|
-      # on the first search there is no change as defined before loop
-      # on the second search it gets narrower etc.
-      # @tag_results = @tag_results & Place.search_by_tag_id(id)
-
-      # @tag_results = @tag_results & Post.search_by_tag_id(id)
-      @tag_results = @tag_results & Post.search_by_tag_name(Tag.find(id).name)
-
+    @tag_posts = Post.search_by_tag_name(@searched_tags.first)
+    @searched_tags.each do |tagname|
+      @tag_posts = @tag_posts & Post.search_by_tag_name(tagname)
     end
-    @tag_results = @tag_results & current_user.post_feed
-
-    @tag_results.map! {|x| x.place}
-    @places = @tag_results
-    # Behaves weirdly for cafe tag
+    @tag_posts = @tag_posts & current_user.post_feed
+    @places = @tag_posts.map(&:place)
   end
-
 end
-
