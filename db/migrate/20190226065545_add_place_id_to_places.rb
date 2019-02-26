20ff253e5b069fbd8f9a71f02931dcbbacdaba4a
vilson junior <vilsonrjunior@gmail.com>
2019-02-26 15:05:35 +0800

    added google_place_id to places schema

---
+++ db/migrate/20190226065545_add_place_id_to_places.rb
@@ -0,0 +1,4 @@
+class AddPlaceIdToPlaces < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_052822) do
+ActiveRecord::Schema.define(version: 2019_02_26_065704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_23_052822) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "google_place_id"
   end
 
   create_table "post_tags", force: :cascade do |t|
