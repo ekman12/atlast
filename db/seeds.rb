68e72e0c3a5c2277cb551f86f21b68a720cad4d7
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-27 09:44:34 +0800

    real seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,69 @@ PLACE_PHOTOS = [
 "https://images.unsplash.com/photo-1515215316771-2742baa337f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60.png"
 ]
 
+PLACE_NAMES = [
+  "Potato Head Beach Club Bali",
+  "Crate Cafe",
+  "Milk & Madu Cafe",
+  "Nude Canggu Cafe",
+  "The Loft Canggu",
+  "Machinery Cafe Bali (MCB)",
+  "Debbie Does Salad",
+  "Peloton Supershop",
+  "Bottega Italiana Canggu",
+  "Coffee & Coconuts",
+  "MOTION Cafe"
+]
+
+PLACE_ADDRESSES = [
+ "Potato Head Beach Club Bali, Jalan Petitenget, Kerobokan Kelod, Badung Regency, Bali, Indonesia",
+  "Crate Cafe, Jalan Canggu Padang Linjong, Canggu, Badung Regency, Bali, Indonesia",
+  "Milk & Madu Cafe, Jalan Pantai Berawa, Tibubeneng, Berawa, Bali, Indonesia",
+  "Nude Canggu Cafe, Jalan Pantai Berawa, Tibubeneng, Badung Regency, Bali, Indonesia",
+  "The Loft Canggu, Jalan Pantai Batu Bolong, Canggu, Badung Regency, Bali, Indonesia",
+  "Machinery Cafe Bali (MCB), Jalan Pantai Batu Bolong, Canggu, Badung Regency, Bali, Indonesia",
+  "Debbie Does Salad, Jalan Pura Batu Mejan, Canggu, Badung Regency, Bali, Indonesia",
+  "Peloton Supershop, Jalan Pantai Berawa, Tibubeneng, Badung Regency, Bali, Indonesia",
+  "Bottega Italiana Canggu, Jalan Pantai Batu Bolong, Canggu, Badung Regency, Bali, Indonesia",
+  "Coffee & Coconuts, Canggu, Badung Regency, Bali, Indonesia",
+  "MOTION Cafe, Jalan Pantai Batu Bolong, Canggu, Badung Regency, Bali, Indonesia"
+]
+
+USERS = [
+  "tomek",
+  "vilson",
+  "caio",
+  "sonia",
+  "guido",
+  "eli",
+  "prima",
+  "peter",
+  "ashwin",
+  "janna",
+  "george",
+  "charles",
+  "lorenzo",
+  "john",
+  "kane",
+  "yejin",
+  "romain"
+]
+
+# PLACE_PHOTOS = [
+  #   "https://cdn.thebalibible.com/uploads/galleries/IMG_1216-lr_1521529204.jpg",
+
+#   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCOuS2MJ9wepjXwEoCTxsEDimjz7Wxf23a8Ir9b5DKwYeW3lxYKg",
+#   "http://static.asiawebdirect.com/m/bangkok/portals/bali-indonesia-com/homepage/magazine/milk-and-madu-cafe/pagePropertiesImage/milk-and-madu.jpg.jpg",
+#   "https://lonelyplanetwp.imgix.net/2018/10/Nude_03-2f12614ffc35.jpg?fit=min&q=40&sharp=10&vib=20&w=1470",
+#   "https://flightstofancy.com/wp-content/uploads/2018/05/The-Loft-Canggu-Shake.png",
+#   "https://media-cdn.tripadvisor.com/media/photo-s/0e/96/3d/54/local.jpg",
+#   "https://lh3.googleusercontent.com/GRLUFlVaDejJkz9q-F_e4l9YbHrneuscEt0dld5jhNUDpETnbwIeK5f_4SzdY3luc4rYd26E=w1080-h608-p-no-v0",
+#   "https://media-cdn.tripadvisor.com/media/photo-s/10/ce/ac/52/the-marathon-breakfast.jpg",
+#   "https://cdn.thebalibible.com/uploads/galleries/18801204475879f85e4b57a_Zibiru_Bottega-Italiana_Bali_28.jpg",
+#   "http://coffeeandcoconuts.com/wp-content/uploads/2016/12/Coffeecoconuts_interior-10-s.jpg",
+#   "https://media-cdn.tripadvisor.com/media/photo-s/06/ba/c2/6e/avocado-cafe.jpg"
+# ]
+
 p 'deleting previous seeds'
 
 PostTag.delete_all
@@ -43,82 +106,83 @@ p '-----'
 
 p 'creating 20 users'
 
-10.times do
+x = 0
+17.times do
   user = User.new(
-        first_name: Faker::Name.first_name,
+        first_name: USERS[x],
         last_name: Faker::Name.last_name,
         picture: "https://randomuser.me/api/portraits/#{['wo',''].sample}men/#{(0..99).to_a.sample}.jpg",
         username: Faker::Internet.username,
-        email: Faker::Internet.unique.email,
+        email: "#{USERS[x]}@test.com",
         password: "123456"
       )
   user.remote_photo_url = user.picture
   user.save!
+  x += 1
 end
 
-p 'created 10 users'
+# p 'created 10 users'
 
-  tomek = User.new(
-        first_name: 'Tomek',
-        last_name: 'Atlast',
-        picture: "https://randomuser.me/api/portraits/#{['wo',''].sample}men/#{(0..99).to_a.sample}.jpg",
-        username: 'Tomek',
-        email: 'tomek@test.com',
-        password: "123456"
-      )
-  tomek.remote_photo_url = tomek.picture
-  tomek.save!
-  tomek.save!
-  vilson = User.new(
-        first_name: 'Vilson',
-        last_name: 'Atlast',
-        picture: "https://randomuser.me/api/portraits/#{['wo',''].sample}men/#{(0..99).to_a.sample}.jpg",
-        username: 'Vilson',
-        email: 'vilson@test.com',
-        password: "123456"
-      )
-  vilson.remote_photo_url = vilson.picture
-  vilson.save!
-  vilson.save!
-  caio = User.new(
-        first_name: 'Caio',
-        last_name: 'Atlast',
-        picture: "https://randomuser.me/api/portraits/#{['wo',''].sample}men/#{(0..99).to_a.sample}.jpg",
-        username: 'Caio',
-        email: 'caio@test.com',
-        password: "123456"
-      )
-  caio.remote_photo_url = caio.picture
-  caio.save!
-  caio.save!
+#   tomek = User.new(
+#         first_name: 'Tomek',
+#         last_name: 'Atlast',
+#         picture: "https://randomuser.me/api/portraits/#{['wo',''].sample}men/#{(0..99).to_a.sample}.jpg",
+#         username: 'Tomek',
+#         email: 'tomek@test.com',
+#         password: "123456"
+#       )
+#   tomek.remote_photo_url = tomek.picture
+#   tomek.save!
+#   tomek.save!
+#   vilson = User.new(
+#         first_name: 'Vilson',
+#         last_name: 'Atlast',
+#         picture: "https://randomuser.me/api/portraits/#{['wo',''].sample}men/#{(0..99).to_a.sample}.jpg",
+#         username: 'Vilson',
+#         email: 'vilson@test.com',
+#         password: "123456"
+#       )
+#   vilson.remote_photo_url = vilson.picture
+#   vilson.save!
+#   vilson.save!
+#   caio = User.new(
+#         first_name: 'Caio',
+#         last_name: 'Atlast',
+#         picture: "https://randomuser.me/api/portraits/#{['wo',''].sample}men/#{(0..99).to_a.sample}.jpg",
+#         username: 'Caio',
+#         email: 'caio@test.com',
+#         password: "123456"
+#       )
+#   caio.remote_photo_url = caio.picture
+#   caio.save!
+  # caio.save!
 
   # NEED TO MAKE SURE THAT WE DO NOT SAVE TWICE
 
-  UserRelationship.new(follower: tomek, followed: vilson).save!
-  UserRelationship.new(follower: tomek, followed: caio).save!
-  UserRelationship.new(follower: tomek, followed: User.first).save!
-  UserRelationship.new(follower: tomek, followed: User.second).save!
-  UserRelationship.new(follower: caio, followed: vilson).save!
-  UserRelationship.new(follower: caio, followed: tomek).save!
-  UserRelationship.new(follower: caio, followed: User.first).save!
-  UserRelationship.new(follower: caio, followed: User.second).save!
-  UserRelationship.new(follower: vilson, followed: tomek).save!
-  UserRelationship.new(follower: vilson, followed: caio).save!
-  UserRelationship.new(follower: vilson, followed: User.first).save!
-  UserRelationship.new(follower: vilson, followed: User.second).save!
+  # UserRelationship.new(follower: tomek, followed: vilson).save!
+  # UserRelationship.new(follower: tomek, followed: caio).save!
+  # UserRelationship.new(follower: tomek, followed: User.first).save!
+  # UserRelationship.new(follower: tomek, followed: User.second).save!
+  # UserRelationship.new(follower: caio, followed: vilson).save!
+  # UserRelationship.new(follower: caio, followed: tomek).save!
+  # UserRelationship.new(follower: caio, followed: User.first).save!
+  # UserRelationship.new(follower: caio, followed: User.second).save!
+  # UserRelationship.new(follower: vilson, followed: tomek).save!
+  # UserRelationship.new(follower: vilson, followed: caio).save!
+  # UserRelationship.new(follower: vilson, followed: User.first).save!
+  # UserRelationship.new(follower: vilson, followed: User.second).save!
 
 p '-----'
 p 'creating 10 places'
 
+x = 0
 10.times do
   place = Place.new(
-    name: Faker::Restaurant.name,
-    # country: Faker::Address.country,
-    # city: Faker::Address.city,
-    # neighbourhood: Faker::Address.community,
-    address: Faker::Address.full_address
+    name: PLACE_NAMES[x],
+    address: PLACE_ADDRESSES[x]
     )
   place.save!
+  x += 1
 
 
 p ' creating posts for the place'
@@ -158,7 +222,7 @@ bar.save!
 
 restaurant = Tag.new(
   tag_type: 'venue',
-  name: 'restaraunt'
+  name: 'restaurant'
   )
 restaurant.save!
 
@@ -294,7 +358,7 @@ p 'created 60 post_tags'
 p '-----'
 p 'creating followers / followed relationship'
 
-40.times do
+60.times do
   followed = User.all.sample
   follower = User.where.not(id: followed.id).sample
   relationship = UserRelationship.new(follower: follower, followed: followed)
