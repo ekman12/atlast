6273191c86e5f9203666e1c941b40125c57d5611
vilson junior <vilsonrjunior@gmail.com>
2019-03-15 09:47:59 +0800

    added omniauthable fb to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
   has_many :wishlist_items
   has_many :posts
   has_many :places, through: :posts
